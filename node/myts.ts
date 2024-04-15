c296cd1cb8496b16d919bb22c91744ccabf92046
DariaEvtina <catbrat02@gmail.com>
2024-04-15 20:19:10 +0300

    typescript test

---
+++ node/myts.ts
@@ -0,0 +1,13 @@
+let x:number=10
+function sum(x:number, y:number):number {
+  return x + y
+}
+let a:string[]=["apple", "banana", "cherry"]
+interface ICars{
+    wheels?: number,
+    color: string | number
+}
+let car:ICars={
+    wheels:5,
+    color:"red"
+}
